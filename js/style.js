a54c9c0fe8d92991c36713ba157890005e22733c
SarahOh92 <201124081oek@gmail.com>
2022-06-29 18:35:47 +0900

    컨텐츠 추가 및 사이즈 수정

--- js/style.js
+++ js/style.js
@@ -2,5 +2,31 @@ $( function() {
     $( "#gnb_tabs" ).tabs({
       event: "mouseover"
     });
-  } 
+    
+}
 );
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
