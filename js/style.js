955f5ab697dc15a6e232caad0688840e21e2451d
SarahOh92 <201124081oek@gmail.com>
2022-07-06 17:15:02 +0900

    네이밍 수정 및 사이즈 수정

--- js/style.js
+++ js/style.js
@@ -3,7 +3,7 @@ $( function() {
       event: "mouseover"
     });
     
-    $('.tabgroup > .tab_cont').hide();
+    $('.tabgroup > .tab_list').hide();
     $('.tabgroup > #tab2').show();
     $('.tabs a').click(function(e){
       e.preventDefault();
@@ -13,33 +13,38 @@ $( function() {
             target = $this.attr('href');
         others.removeClass('active');
         $this.addClass('active');
-        $(tabgroup).children('.tab_cont').hide();
+        $(tabgroup).children('.tab_list').hide();
         $(target).show();
     })
 }
 );
-$('.center').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-});
+onload=function(){
+};
+var m3=[
+    {m:"01월",v:300000}
+    ,{m:"02월",v:0}
+    ,{m:"03월",v:200000}
+    ,{m:"04월",v:400000}
+    ,{m:"05월",v:100000}
+    ,{m:"06월",v:300000}
+],chart3=AmCharts.makeChart("lchart", {
+    type:'serial',
+    startDuration:1,
+    color:'#888',
+    colors:['#F79185'],
+    dataProvider:m3,
+    dataDateFormat:'MM',
+    categoryField:'m',
+    graphs:[
+        {valueField:'v',labelText:'[[value]]',bullet:'round',bulletBorderAlpha:1,bulletColor:'#FFF',bulletSize:10,useLineColorForBulletBorder:true,balloonText:"[[category]]월 : [[value]]"}
+    ],
+    chartCursor:{categoryBalloonDateFormat:'MM',cursorColor:'#F79185'},
+    valueAxes:[{axisAlpha:0,gridAlpha:.05,labelsEnabled:false}],
+    categoryAxis:{axisAlpha:0,gridAlpha:0}
+}),menu=function(o,n,c){
+    o.className='';
+    n.className=c;
+},mc=function(e,el){
+    if(e.clientY<40)location='mtodo.htm';
+    else if(e.clientY<100)menu(el,el.nextSibling,'on2')
+};
